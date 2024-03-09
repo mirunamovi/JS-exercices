c3a5888224628543803028b289e5da62657d887e
Movileanu Miruna <mirunamovi@gmail.com>
2024-03-09 21:27:25 +0200

    Progress

--- js-note/main.js
+++ js-note/main.js
@@ -7,12 +7,19 @@ const description = document.getElementById('description');
 const icons = document.getElementById('icons');
 const newNote = document.getElementById('creating-note');
 const submit = document.getElementById('submit');
+
 let isToggled = false;
 let noteStored = [];
+
 const template = document.querySelector('template').content;
 const h3 = template.querySelector('h2');
 const p = template.querySelector('p');
 const element = template.querySelector('.note-area-content');
+
+let isClicked = false;
+let checkedIcon_prev = null;
+let checkedIcons_prev = null;
+
 
 document.addEventListener('DOMContentLoaded', function () {
   init();
@@ -23,6 +30,7 @@ function init() {
   getStoredData();
   noteStored.forEach(function (note) {
     renderNote(note);
+
   });  
   bindEvents(); 
 
@@ -48,8 +56,14 @@ function changeInputOnToggleState(title, newNote, icons) {
       icons.style.display = 'none';
   } else {
       title.style.display = 'block';
-      newNote.style.height = '138px';
+      title.style.border = 'none';
+      title.style.paddingBottom = '30px';
+      newNote.style.height = 'auto';
+      newNote.style.border = '2px solid black';
+      newNote.style.borderRadius = '11px';
+      title.style.paddingTop = '20px';
       icons.style.display = 'block';
+      icons.style.paddingBottom = '10px';
   }
 }
 
@@ -57,12 +71,21 @@ function getNodeData(){
 
   const titleNode = title.value;
   const descriptionNode = description.value;
-
-  const noteData = { titleNode, descriptionNode};
+  const idNode = guid();
+  const noteData = { idNode, titleNode, descriptionNode};
 
   resetInputFields()
   
   return noteData;
+}
+
+function guid() {
+  function _p8(s) {
+      var p = (Math.random().toString(16) + "000000000").substr(2, 8);
+      return s ? "-" + p.substr(0, 4) + "-" + p.substr(4, 4) : p;
+  }
+ return 'a' + _p8() + _p8(true) + _p8(true) + _p8();
+
 }
 
 function resetInputFields() {
@@ -77,6 +100,8 @@ function saveNode(){
     noteStored.push(note);
     localStorage.setItem('notes', JSON.stringify(noteStored));
   }
+  renderNote(note);
+
 }
 
 function renderNote(data) {
@@ -85,16 +110,20 @@ function renderNote(data) {
 
   const newNoteElement = document.importNode(templateContent, true);
 
+  const idElement = newNoteElement.querySelector('.note-area-content');
   const titleElement = newNoteElement.querySelector('.note-area-content-title');
   const bodyElement = newNoteElement.querySelector('.note-area-content-body');
 
+ 
+
+  idElement.id = data.idNode;
   titleElement.textContent = data.titleNode;
   bodyElement.textContent = data.descriptionNode;
 
   document.querySelector('#notes').appendChild(newNoteElement);
+  document.querySelector('#' + data.idNode).addEventListener('click', handleElementClick);
+
 }
-
-
 
 function getStoredData() {
   noteStored = JSON.parse(localStorage.getItem('notes') || '[]');
@@ -105,6 +134,45 @@ function renderStoredNotes() {
       renderNote(note);
   });
 }
+
+function handleElementClick(ev){
+  ev.stopPropagation();  // sa nu faca click pe body
+
+  let v = ev.target.parentNode;
+  if(v.id === "footer"){
+    v = ev.target.parentNode.parentNode;
+  }
+  console.log(v);
+
+  // console.log(ev.target);
+  // console.log(ev.target.parentNode.parentNode);
+
+ const checkedIcon = v.querySelector('.hid');
+ const checkedIcons = v.querySelectorAll('.hidd');
+
+ //console.log(checkedIcon);
+ //console.log(checkedIcon_prev);
+  if((checkedIcon_prev != null) && (checkedIcons_prev != null) &&(checkedIcon != null) && (checkedIcons != null))  {
+    checkedIcon_prev.style.display = 'none';
+    // checkedIcons_prev.style.display = 'none';
+    for( let value of checkedIcons_prev){
+      value.style.display = 'none';
+    }
+
+
+  }
+  checkedIcon_prev = checkedIcon;
+  checkedIcon.style.display = 'block'; 
+  checkedIcons_prev = checkedIcons;
+  // checkedIcons[1].style.display = 'block'; 
+  for( let value of checkedIcons){
+    value.style.display = 'inline';
+  }
+  // checkedIcons.map((value) => {value.style.display = 'block';})
+
+}
+
+
 
 function bindEvents(){
   newNote.addEventListener('click', handleNewNoteClick); 
@@ -119,6 +187,13 @@ function bindEvents(){
 
   function handlePageBodyClick(ev){
     console.log("Hello3");
+    if((checkedIcon_prev != null) && (checkedIcons_prev != null)) {
+      checkedIcon_prev.style.display = 'none';
+      // checkedIcons_prev.style.display = 'none';
+      for( let value of checkedIcons_prev){
+        value.style.display = 'none';
+      }
+    }
     ev.stopPropagation();
     toggle(false);
   }
@@ -127,7 +202,10 @@ function bindEvents(){
     ev.stopPropagation();
     saveNode();
     toggle(false);
-  }  
+  } 
+  
 
+
+  
 }
 
